a347e5ee46464904b8675cf2fcbfec9fe19acf66
Jens-Alrik Adrian <jensalrik.adrian@gmail.com>
2022-03-31 19:46:37 +0200

    Register page

--- market/__init__.py
+++ market/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'abb3c74d3a4a6fbb0e25ecba'
 
 db = SQLAlchemy(app)
 
